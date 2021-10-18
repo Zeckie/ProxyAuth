3bfdbfc5a92d287f7be22d4a58ec53b0e49d5ce7
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-10-19 07:34:45 +1000

    add qodana

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    id("org.jetbrains.qodana") version "0.1.12"
 }
 
 version = "0.1.0"
@@ -21,4 +22,12 @@ java {
     tasks.jar {
         manifest.attributes["Main-Class"] = main
     }
+}
+
+// Configure Gradle Qodana Plugin
+qodana {
+    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
+    reportPath.set(buildDir.resolve("reports/inspections").canonicalPath)
+    saveReport.set(true)
+    showReport.set(System.getenv("QODANA_SHOW_REPORT").toBoolean())
 }
