179ab288ec1b3cc4f6f7f14617cbb7b0587e0ad4
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-10-21 16:44:38 +1000

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,57 +3,57 @@ plugins {
     id("org.jetbrains.changelog") version "1.3.1"
 }
 
+version = "0.1.1"
+group = "com.github.zeckie"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    implementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+val minimumJavaVersion = JavaVersion.VERSION_1_9
 
-version = "0.1.1"
-group = "com.github.zeckie"
-
-// Work out current java version
-val MIN_JAVA_VER = 9
-val fullVersion = System.getProperty("java.version")
-val majorVersion = Integer.parseInt(fullVersion.substring(0, fullVersion.indexOf('.')))
-System.out.println("Java major version: " + majorVersion)
-
-if (majorVersion < MIN_JAVA_VER) {
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(MIN_JAVA_VER))
-        }
-    }
+if (JavaVersion.current() < minimumJavaVersion) {
+    java.toolchain.languageVersion.set(JavaLanguageVersion.of(minimumJavaVersion.majorVersion))
 } else {
-    if (majorVersion > 9) {
+    if (JavaVersion.current() > JavaVersion.VERSION_1_9) {
         // Compile for minimum supported java version
         // skip for java 9 due to https://bugs.openjdk.java.net/browse/JDK-8139607
         tasks.withType<JavaCompile> {
-            options.release.set(MIN_JAVA_VER)
+            options.release.set(minimumJavaVersion.majorVersion.toInt())
         }
     }
 
-    // Test on minimum supported java version
-    tasks.register<Test>("testsMinJava") {
-        javaLauncher.set(javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(MIN_JAVA_VER))
-        })
+    tasks.check {
+        dependsOn(testsMinJava)
     }
-    tasks.build {
-        dependsOn("testsMinJava")
-    }
+}
+
+// Test on minimum supported java version
+val testsMinJava by tasks.registering(Test::class) {
+    description = "Run test suite with ${project.name}'s minimum supported java version ($minimumJavaVersion). " +
+            "Checks that the code is compatible with, and compiled with the correct options to be run on that version"
+    group = "verification"
+
+    javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(minimumJavaVersion.majorVersion))
+    })
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 // set main class
 tasks.jar {
     manifest.attributes["Main-Class"] = "proxyauth.Main"
+}
+
+tasks.processResources {
+    with(copySpec().from("LICENSE").into("proxyauth"))
 }
 
 changelog {
