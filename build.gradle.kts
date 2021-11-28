a3f5c288cbcda8099afbd8ce14d96aeb6c22f42b
Zeckie <49095968+Zeckie@users.noreply.github.com>
2021-11-28 12:47:38 +1000

    Gradle build using Java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,20 @@ tasks.test {
 version = "0.1.1"
 group = "com.github.zeckie"
 
+// Work out current java version
 val MIN_JAVA_VER = 9
+val fullVersion = System.getProperty("java.version")
+val majorVersion = Integer.parseInt(fullVersion.substring(0, fullVersion.indexOf('.')))
+System.out.println("Java major version: " + majorVersion)
 
-if (Runtime.version().feature() < MIN_JAVA_VER) {
+if (majorVersion < MIN_JAVA_VER) {
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(MIN_JAVA_VER))
         }
     }
 } else {
-    if (Runtime.version().feature() > 9) {
+    if (majorVersion > 9) {
         // Compile for minimum supported java version
         // skip for java 9 due to https://bugs.openjdk.java.net/browse/JDK-8139607
         tasks.withType<JavaCompile> {
